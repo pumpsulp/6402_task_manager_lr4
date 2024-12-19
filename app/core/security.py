84a3f39f6c5bc41c00b4c90a6f72660a2debbbd2
X6uddy <sashavolkov_03@mail.ru>
2024-12-19 23:03:24 +0400

    [auth]-added cookie methods

--- app/core/security.py
+++ app/core/security.py
@@ -61,3 +61,63 @@ def decode_jwt_token(token: str) -> dict:
     """
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     return payload
+
+
+async def set_cookie_with_token(response: Response, token: str):
+    """
+    Добавление в Response Cookie с JWT-токеном
+    :param response: Response (HTTP-ответ)
+    :param token: зашифрованный JWT-токен
+    :return:
+    """
+    response.set_cookie(
+        key="access_token",
+        value=token,
+        httponly=True,  # Защита от CRSF
+        max_age=ACCESS_TOKEN_EXPIRE_SECONDS,
+        expires=ACCESS_TOKEN_EXPIRE_SECONDS,
+        samesite="lax",
+        secure=True
+    )
+
+
+def get_user_id_from_token(payload: dict) -> int:
+    """
+    Получает user_id из полезной нагрузки токена (payload).
+    Если user_id отсутствует, бросает HTTPException с 401 статусом.
+    :param payload: полезная нагрузка
+    """
+    user_id: str = payload.get("sub")
+    if user_id is None:
+        raise not_authorized_exception
+    return int(user_id)
+
+
+async def get_token_from_cookie(request: Request):
+    """
+    Получение зашифрованного JWT-токена из Cookie HTTP-запроса
+    :param request: HTTP-запрос
+    :return:
+    """
+    token = request.cookies.get("access_token")
+    if not token:
+        raise HTTPException(status_code=401, detail="Can't get token")
+    return token
+
+
+async def get_current_user_id(request: Request) -> int:
+    """
+    Основная функция для получения текущего пользователя из Cookie.
+    Декодирует токен JWT и возвращает user_id.
+    :param request: HTTP-запрос
+    """
+    try:
+        token = request.cookies.get("access_token")
+        if token is None:
+            raise not_authorized_exception
+    
+        payload = decode_jwt_token(token)
+        user_id = get_user_id_from_token(payload)
+        return user_id
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=str(e))
